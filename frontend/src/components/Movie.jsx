2af8107f4370c8f1515c2d1240d563c2da44efac
JuanSilva27 <juan.silva.0270@gmail.com>
2022-02-08 18:03:59 -0300

    se agregan apis al proyecto de React

---
+++ frontend/src/components/Movie.jsx
@@ -0,0 +1,76 @@
+import React, { Component } from 'react';
+
+import axios from "axios"
+import MovieList from './MovieList';
+
+
+
+class Movie extends Component {
+
+	constructor() {
+		super()
+		this.state = {
+			movies: ""
+		}
+	}
+
+	componentDidMount() {
+		axios("http://localhost:3001/api/movies")
+			.then(response => {
+				this.setState({ movies: response.data.data })
+			})
+	}
+
+	render() {
+		return (
+			<React.Fragment>
+				{/*<!-- PRODUCTS LIST -->*/}
+				<h1 className="h3 mb-2 text-gray-800">All the movies in the Database</h1>
+
+				{/*<!-- DataTales Example -->*/}
+				<div className="card shadow mb-4">
+					<div className="card-body">
+						<div className="table-responsive">
+							<table className="table table-bordered" id="dataTable" width="100%" cellSpacing="0">
+								<thead>
+									<tr>
+										<th>Id</th>
+										<th>Titulo</th>
+										<th>Calificación</th>
+										<th>Premios</th>
+										<th>Duración</th>
+									</tr>
+								</thead>
+								<tfoot>
+									<tr>
+										<th>Id</th>
+										<th>Titulo</th>
+										<th>Calificación</th>
+										<th>Premios</th>
+										<th>Duración</th>
+									</tr>
+								</tfoot>
+								<tbody>
+									{
+										this.state.movies !== "" ? this.state.movies.map(movie =>
+											<MovieList
+												key={movie.id}
+												id={movie.id}
+												title={movie.title}
+												rating={movie.rating}
+												awards={movie.awards}
+												length={movie.length}
+
+											/>)
+											: null
+									}
+								</tbody>
+							</table>
+						</div>
+					</div>
+				</div>
+			</React.Fragment>
+		)
+	}
+}
+export default Movie;
